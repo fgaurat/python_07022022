dcb95390b2c1e926aee854dfab6aaeb646e0b294
Frédéric Gaurat <fgaurat@eolem.com>
2022-02-09 11:29:42 +0100

    add write file

---
+++ ch07/main_file.py
@@ -0,0 +1,22 @@
+
+
+
+
+def main():
+    nom_fichier = "le_fichier.txt"
+    
+    
+    # ma_liste = ['Valeur 01','Valeur 02','Valeur 03','Valeur 04','Valeur 05']
+    
+    ma_liste = [f"Valeur {v:02}" for v in range(1,15)]
+
+    
+    print(ma_liste)    
+    with open(nom_fichier,'w') as f:
+        print("Hello world",file=f)
+        print("Hello world",file=f)
+        print("Hello world",file=f)
+
+
+if __name__ == '__main__':
+    main()
