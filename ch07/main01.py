d1610b7747c73cf304ab3d645188b2a5d8b246b4
Frédéric Gaurat <fgaurat@eolem.com>
2022-02-09 10:29:13 +0100

    add format method

--- ch07/main01.py
+++ ch07/main01.py
@@ -10,9 +10,11 @@ def main():
     k = "id"
     s = f'id : {d[k]},title:{d["title"]}'
     print(s)
-    
+
     # s= "id: {0}, title: {1}".format(d['id'], d['title']) 
     s= "id: {}, title: {}".format(d['id'], d['title'])# respecte l'ordre
+    print(s)
+    s= "id: {the_id}, title: {the_title}".format(the_id=d['id'], the_title=d['title'])# respecte l'ordre
     print(s)
 
 
