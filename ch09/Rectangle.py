437994d4ab7c051c12df9932f29c5c5dcc273a0e
Frédéric Gaurat <fgaurat@eolem.com>
2022-02-09 16:31:30 +0100

    add propert/usr/local/Cellar/python@3.9/3.9.9/Frameworks/Python.framework/Versions/3.9/bin/python3.9 /Users/fgaurat/local_dev/formations/python/ch09/main.pyy ch09 object

--- ch09/Rectangle.py
+++ ch09/Rectangle.py
@@ -4,5 +4,13 @@
 class Rectangle:
 
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,longueur=0,largeur=0) -> None:
+        print(f"def __init__(self,{longueur},{largeur})")
+        self._longueur = longueur
+        self._largeur = largeur
+    
+    def get_longueur(self):
+        return self._longueur
+
+    def get_largeur(self):
+        return self._largeur
--- ch09/main.py
+++ ch09/main.py
@@ -1,12 +1,15 @@
+from Rectangle import Rectangle
 
 def main():
     
     # Construire objet r à partir de la classe Rectangle
     r = Rectangle(2,3)# longueur 2 et largeur 3
+    # r1 = Rectangle()# longueur 2 et largeur 3
 
+    
     print(r.get_largeur()) # 3
-    r.set_largeur(5)
-    print(r.get_largeur()) # 5
+    # r.set_largeur(5)
+    # print(r.get_largeur()) # 5
 
 if __name__ == '__main__':
     main()
