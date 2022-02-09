754dac80ce6bac7a9500eb5e826473594f818b32
Frédéric Gaurat <fgaurat@eolem.com>
2022-02-09 11:58:10 +0100

    add data

--- ch07/main_file.py
+++ ch07/main_file.py
@@ -1,20 +1,54 @@
 
-
-
-
 def main():
+    data = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": False
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": False
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": False
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": True
+        },
+    ]
+
+
+def main_read_file():
     nom_fichier = "le_fichier.txt"
-    
-    
+
+    # with open(nom_fichier,'r') as f:
+    with open(nom_fichier) as f:
+        for line in f:
+            # print(line,end="")
+            print(line.strip())
+
+
+def main_write_file():
+    nom_fichier = "le_fichier.txt"
+
     # ma_liste = ['Valeur 01','Valeur 02','Valeur 03','Valeur 04','Valeur 05']
-    
-    ma_liste = [f"Valeur {v:02}" for v in range(1,15)]
 
+    ma_liste = [f"Valeur {v:02}" for v in range(1, 15)]
 
-    print(ma_liste)    
-    with open(nom_fichier,'w') as f:
+    print(ma_liste)
+    with open(nom_fichier, 'w') as f:
         for line in ma_liste:
-            print(line,file=f)
+            print(line, file=f)
 
 
 if __name__ == '__main__':
