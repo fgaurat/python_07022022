abe7eca0da0a84f932f0b6d761225c4b2915038e
Frédéric Gaurat <fgaurat@eolem.com>
2022-02-11 14:07:00 +0100

    ch10_1 DAO ok

--- ch10_01/main.py
+++ ch10_01/main.py
@@ -2,20 +2,34 @@ import requests
 
 from Todo import Todo
 from TodoDAO import TodoDAO
+import configparser
+import argparse
 
 def main():
-    url = "http://jsonplaceholder.typicode.com/todos"
+    parser = argparse.ArgumentParser(description='TP read and write DB.')
+    parser.add_argument('config_file',help="Configuration file")
+    args = parser.parse_args()
     
-    r = requests.get(url)
-    todos = r.json()
+    config = configparser.ConfigParser()
+    config.read(args.config_file)
 
-    dao = TodoDAO("todos_database.db")
-    for todo in todos:
-        # obj = Todo(id=todo['id'],title=todo['title'],...)
-        obj = Todo(**todo)
-        print(obj)
-        dao.save(obj)
-        
+
+    data_file = config['DB']['data_file']
+    # url = "http://jsonplaceholder.typicode.com/todos"
+    # r = requests.get(url)
+    # todos = r.json()
+
+    dao = TodoDAO(data_file)
+    # for todo in todos:
+    #     # obj = Todo(id=todo['id'],title=todo['title'],...)
+    #     obj = Todo(**todo)
+    #     print(obj)
+    #     dao.save(obj)
+
+    list_todos = dao.findAll()
+
+    for todo in list_todos:
+        print(todo)
 
 
 if __name__ == '__main__':
